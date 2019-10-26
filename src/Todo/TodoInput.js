a1ea907d9784303837d82becb97389d7690ed775
Glitch (orthodex-awaaz) <none>
2019-10-26 05:23:10 +0000

    🐣🤗 Checkpoint ./package.json:3680637/132 ./src/Todo/TodoInput.js:3680637/3944

--- src/Todo/TodoInput.js
+++ src/Todo/TodoInput.js
@@ -7,13 +7,38 @@ import {
   MUTATION_TODO_ADD
 } from './graphQueries/todoQueries';
 
+import { ReactMic } from "react-mic";
+import AudioPlayer from "react-modular-audio-player";
+
 export default class TodoInput extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      textboxValue: ''
+      record: false,
+      recordedBlob: Blob()
     }
+  }
+
+  startRecording = () => {
+    this.setState({
+      record: true
+    });
+  }
+
+  stopRecording = () => {
+    this.setState({
+      record: false
+    });
+  }
+
+  // onData(recordedBlob) {
+  //   console.log('chunk of real-time data is: ', recordedBlob);
+  // }
+
+  onStop(recordedBlob) {
+    this.setState({recordedBlob})
+    console.log('recordedBlob is: ', recordedBlob);
   }
 
   handleTextboxValueChange = (e) => {
@@ -56,12 +81,28 @@ export default class TodoInput extends React.Component {
         {(addTodo, { data, loading, called, error }) => {
           return (
             <div className="parentContainer">
-              <input className="input" placeholder="Add a todo" value={this.state.textboxValue} onChange={this.handleTextboxValueChange} onKeyPress={e => {
+              {/* <input className="input" placeholder="Add a todo" value={this.state.textboxValue} onChange={this.handleTextboxValueChange} onKeyPress={e => {
                   this.handleTextboxKeyPress(e, addTodo);
-                }}/>
-              <input className="input" placeholder="Add a todo" value={this.state.textboxValue} onChange={this.handleTextboxValueChange} onKeyPress={e => {
-                  this.handleTextboxKeyPress(e, addTodo);
-                }}/>
+                }}/> */}
+              <button onClick={this.state.record ? this.stopRecording : this.startRecording }>Toggle Recording</button>
+              <p>{this.state.record ? "Recording!" : "Not Recording"}</p>
+              <ReactMic
+                record={this.state.record}         // defaults -> false.  Set to true to begin recording
+                // pause={boolean}          // defaults -> false.  Available in React-Mic-Plus upgrade only
+                // className={string}       // provide css class name
+                onStop={this.onStop}        // callback to execute when audio stops recording
+                // onData={this.onData}        // callback to execute when chunk of audio data is available
+                strokeColor="#000000"     // sound wave color
+                backgroundColor="#FF4081" // background color
+              />
+              <AudioPlayer 
+                audioFiles={[
+                  {
+                    src: this.state.recordedBlob.blobUrl,
+                    title: "Audio recording"
+                  }
+                ]}
+              />
               <br />
             </div>
           )
