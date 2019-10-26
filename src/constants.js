1d776142c67862c421c988b8965636dfe4506226
Glitch (orthodex-awaaz) <none>
2019-10-26 05:00:10 +0000

    🐏🏛 Checkpoint ./src/Auth/auth0-variables.js:3680637/94 ./src/constants.js:3680637/196 ./hasura/config.yaml:3680637/127

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const GRAPHQL_URL = 'https://awaaz-graphql-backend.herokuapp.com/v1/graphql';
+export const authClientId = "cP636nNwH8gvJIZoEOxHtvy0RMLn4Qi4";
+export const authDomain = "orthodex-testing.auth0.com";
