c4ab442accfc59734184fd2455d2bc1ced67295e
Glitch (orthodex-awaaz) <none>
2019-10-26 05:34:11 +0000

    📼👾 Checkpoint ./src/Todo/TodoInput.js:3680637/1661

--- src/Todo/TodoInput.js
+++ src/Todo/TodoInput.js
@@ -1,79 +1,80 @@
-import React from 'react';
+import React from "react";
 import { Mutation } from "react-apollo";
-import './Todo.css';
+import "./Todo.css";
 
-import {
-  QUERY_TODO,
-  MUTATION_TODO_ADD
-} from './graphQueries/todoQueries';
+import { QUERY_TODO, MUTATION_TODO_ADD } from "./graphQueries/todoQueries";
 
 import { ReactMic } from "react-mic";
 import AudioPlayer from "react-modular-audio-player";
 
 export default class TodoInput extends React.Component {
-
   constructor() {
     super();
     this.state = {
       record: false,
-      recordedBlob: Blob()
-    }
+      recordedBlob: new Blob(),
+      recordComplete: false
+    };
+    this.onStop = this.onStop.bind(this);
   }
 
   startRecording = () => {
     this.setState({
       record: true
     });
-  }
+  };
 
   stopRecording = () => {
     this.setState({
-      record: false
+      record: false,
+      recordComplete: true
     });
-  }
+  };
 
   // onData(recordedBlob) {
   //   console.log('chunk of real-time data is: ', recordedBlob);
   // }
 
-  onStop(recordedBlob) {
-    this.setState({recordedBlob})
-    console.log('recordedBlob is: ', recordedBlob);
-  }
+  onStop = recordedBlob => {
+    this.setState({ recordedBlob });
+    console.log("recordedBlob is: ", recordedBlob);
+  };
 
-  handleTextboxValueChange = (e) => {
+  handleTextboxValueChange = e => {
     this.setState({
       textboxValue: e.target.value
     });
-  }
+  };
 
   handleTextboxKeyPress = (e, addTodo) => {
-    if (e.key === 'Enter') {
+    if (e.key === "Enter") {
       const newTask = this.state.textboxValue;
       const userId = this.props.userId;
       addTodo({
         variables: {
-          objects: [{
-            task: newTask,
-            user_id: userId,
-            completed: false
-          }]
+          objects: [
+            {
+              task: newTask,
+              user_id: userId,
+              completed: false
+            }
+          ]
         },
-        update: (store, { data: { insert_todo }}) => {
-          const data = store.readQuery({ query: QUERY_TODO })
+        update: (store, { data: { insert_todo } }) => {
+          const data = store.readQuery({ query: QUERY_TODO });
           const insertedTodo = insert_todo.returning;
-          data.todo.splice(0, 0, insertedTodo[0])
+          data.todo.splice(0, 0, insertedTodo[0]);
           store.writeQuery({
             query: QUERY_TODO,
             data
-          })
+          });
           this.setState({
-            textboxValue: ''
+            textboxValue: ""
           });
         }
-      })
+      });
     }
-  }
+  };
 
   render() {
     return (
@@ -84,30 +85,40 @@ export default class TodoInput extends React.Component {
               {/* <input className="input" placeholder="Add a todo" value={this.state.textboxValue} onChange={this.handleTextboxValueChange} onKeyPress={e => {
                   this.handleTextboxKeyPress(e, addTodo);
                 }}/> */}
-              <button onClick={this.state.record ? this.stopRecording : this.startRecording }>Toggle Recording</button>
+              <button
+                onClick={
+                  this.state.record ? this.stopRecording : this.startRecording
+                }
+              >
+                Toggle Recording
+              </button>
               <p>{this.state.record ? "Recording!" : "Not Recording"}</p>
               <ReactMic
-                record={this.state.record}         // defaults -> false.  Set to true to begin recording
+                record={this.state.record} // defaults -> false.  Set to true to begin recording
                 // pause={boolean}          // defaults -> false.  Available in React-Mic-Plus upgrade only
                 // className={string}       // provide css class name
-                onStop={this.onStop}        // callback to execute when audio stops recording
+                onStop={this.onStop} // callback to execute when audio stops recording
                 // onData={this.onData}        // callback to execute when chunk of audio data is available
-                strokeColor="#000000"     // sound wave color
+                strokeColor="#000000" // sound wave color
                 backgroundColor="#FF4081" // background color
               />
-              <AudioPlayer 
-                audioFiles={[
-                  {
-                    src: this.state.recordedBlob.blobUrl,
-                    title: "Audio recording"
-                  }
-                ]}
-              />
+              {this.state.recordComplete ? (
+                <AudioPlayer
+                  audioFiles={[
+                    {
+                      src: this.state.recordedBlob.blobUrl,
+                      title: "Audio recording"
+                    }
+                  ]}
+                />
+              ) : (
+                ""
+              )}
               <br />
             </div>
-          )
+          );
         }}
       </Mutation>
-    )
+    );
   }
 }
